18e520a0022ed9a1062296ab7eb9abfba4d41087
Taciano <maximotco@hotmail.com>
2023-04-15 15:29:33 -0300

    Update MainView.swift

--- EvercodeApp/Screens/Main/MainView.swift
+++ EvercodeApp/Screens/Main/MainView.swift
@@ -3,9 +3,11 @@ import UIKit
 final class MainView: UIView {
         
     lazy var tableView: UITableView = {
-       
         let view = UITableView()
         view.translatesAutoresizingMaskIntoConstraints = false
+        view.delegate = self
+        view.dataSource = self
+        view.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         return view
     }()
     
@@ -56,5 +58,21 @@ private extension MainView {
             tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
             tableView.bottomAnchor.constraint(equalTo: bottomAnchor)
         ])
+    }
+}
+
+extension MainView: UITableViewDelegate {
+    
+}
+
+extension MainView: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
     }
 }
